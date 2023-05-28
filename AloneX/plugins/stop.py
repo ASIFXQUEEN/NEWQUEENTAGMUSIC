f11981d96c0868604b8243f1535bc32ba6d9bc22
AloneXBot <132041403+AloneXBot@users.noreply.github.com>
2023-05-28 16:02:50 +0530

    Update stop.py

--- AloneX/plugins/stop.py
+++ AloneX/plugins/stop.py
@@ -23,7 +23,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Vip.stop_stream(chat_id)
+    await Alone.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_9"].format(message.from_user.first_name),
