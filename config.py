81f806419a971e0b160e50fb2c403e69b3a0c675
AloneXBot <132041403+AloneXBot@users.noreply.github.com>
2023-05-31 00:01:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -66,7 +66,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL","https://t.me/AloneXBots")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/AlonesHeaven")
 SUPPORT_HEHE = SUPPORT_GROUP
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
-AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "True")
 
 # Time after which you're assistant account will leave chats automatically.
 AUTO_LEAVE_ASSISTANT_TIME = int(
