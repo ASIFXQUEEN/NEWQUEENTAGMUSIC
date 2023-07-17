9fb435876b4c85cfe5257562ea9b536a93794e7b
AloneXBot <132041403+AloneXBot@users.noreply.github.com>
2023-07-17 10:56:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -97,7 +97,7 @@ STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
 
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
 
-YOUTUBE_IMG_URL = "AloneX/assets/alonex.png"
+YOUTUBE_IMG_URL = "AloneX/assets/aanonx.png"
 
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
 
