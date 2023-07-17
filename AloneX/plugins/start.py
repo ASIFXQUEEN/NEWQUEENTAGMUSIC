61699281838fe91bb0437f27219165e43782b532
AloneXBot <132041403+AloneXBot@users.noreply.github.com>
2023-07-17 17:49:10 +0530

    Update start.py

--- AloneX/plugins/start.py
+++ AloneX/plugins/start.py
@@ -43,7 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAx0CX5hlRAACRfRktCYLtJlO_RcGduhKSlvYXxKBSQACzAkAAsOyoVXp1wJmxpUwTy8E")
+            await message.reply_sticker("CAACAgUAAxkBAAEJt35ktTDlpHHx0ASdT5hxriTbhBnYywACzAkAAsOyoVXp1wJmxpUwTy8E")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
@@ -208,7 +208,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAxkBAAIjTGKPYCq3keRZgNbshxtJ5k7H609OAAIZBgACYAF5VIerYoMcSln8JAQ")
+                await message.reply_sticker("CAACAgUAAxkBAAEJt35ktTDlpHHx0ASdT5hxriTbhBnYywACzAkAAsOyoVXp1wJmxpUwTy8E")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
