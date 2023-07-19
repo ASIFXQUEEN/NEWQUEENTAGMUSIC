f5782b889e8aadce6f9bef2a4d19841d17834ff3
TeamAloneOp <131831332+TeamAloneOp@users.noreply.github.com>
2023-07-19 23:40:39 +0530

    Update thumbnails.py

--- AloneX/utils/thumbnails.py
+++ AloneX/utils/thumbnails.py
@@ -4,7 +4,6 @@ import textwrap
 
 import aiofiles
 import aiohttp
-import numpy as np
 
 from PIL import Image, ImageChops, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from youtubesearchpython.__future__ import VideosSearch
