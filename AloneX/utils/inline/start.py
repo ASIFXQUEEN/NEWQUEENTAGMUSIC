20b24fada7abc1cfde34f0bc7d9dd6aca0a6a2d7
TeamAloneOp <131831332+TeamAloneOp@users.noreply.github.com>
2023-08-24 21:05:06 +0530

    Update start.py

--- AloneX/utils/inline/start.py
+++ AloneX/utils/inline/start.py
@@ -8,14 +8,17 @@ import config
 def start_pannel(_, BOT_USERNAME):
     buttons = [
         [
-            InlineKeyboardButton(text="🥀 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 ✨", callback_data="settings_back_helper"),
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=new",
         ],
         [
-            InlineKeyboardButton(text="𝐒𝐮𝐩𝐩𝐨𝐫𝐭 💬", url=config.SUPPORT_GROUP),
-            InlineKeyboardButton(text="📡 𝐔𝐩𝐝𝐚𝐭𝐞𝐬", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text="⚙ 𝐁𝐨𝐭 𝐒𝐞𝐭𝐭𝐢𝐧𝐠 ⚙", callback_data="settings_helper"),
+            InlineKeyboardButton(
+               text=_["S_B_4"], callback_data="settings_back_helper"),
         ],
         ]
     return buttons
@@ -24,22 +27,20 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="➕ ❰ 𝐀𝐝𝐝 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩 ❱ ➕",
+                text=_["S_B_3"],
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=new",
             )
         ],
         [
-            InlineKeyboardButton(text="💖 𝐇𝐞𝐥𝐩 💖", callback_data="settings_back_helper"),
+           InlineKeyboardButton(
+               text=_["S_B_4"], callback_data="settings_back_helper"),
+        [
+            InlineKeyboardButton(text=_["S_B_5"], user_id=OWNER),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
         [
-            InlineKeyboardButton(text="🥀 𝐆𝐫𝐨𝐮𝐩 💥", url=config.SUPPORT_GROUP),
-            InlineKeyboardButton(text="🥀 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 💥", url=config.SUPPORT_CHANNEL),
-        ],
-        [
-            InlineKeyboardButton(text="❄️ 𝐒𝐨𝐮𝐫𝐜𝐞 ❄️", url=config.GITHUB_REPO),
-        ],
-        [
-            InlineKeyboardButton(text="♕ 𝐎𝐰𝐧𝐞𝐫 ♕", user_id=OWNER),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_7"], , url=config.GITHUB_REPO
         ],
     ]
     return buttons
