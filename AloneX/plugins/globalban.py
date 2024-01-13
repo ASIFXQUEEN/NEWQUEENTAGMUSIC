377d4322eb9dbc826553d2edc8d1953a0b8ca202
RAK RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2024-01-13 12:42:27 +0530

    Update globalban.py

--- AloneX/plugins/globalban.py
+++ AloneX/plugins/globalban.py
@@ -67,7 +67,7 @@ async def gbanuser(client, message: Message, _):
         except Exception:
             pass
     await add_banned_user(user_id)
-    await message.reply_sticker("CAACAgUAAxkBAAIjZmKPbsuJzL3TVFQ7q2lc_rRuqa6xAAIyCQACHjuBVOwXUJB64QeSJAQ")
+    await message.reply_sticker("CAACAgUAAxkBAAELKqVlojds6hKdx4JSESXI0B7OvfeTYAACcAYAAlDWyVSevja3qth4qjQE")
     await message.reply_text(
         _["gban_6"].format(mention, number_of_chats)
     )
@@ -111,7 +111,7 @@ async def gungabn(client, message: Message, _):
         except Exception:
             pass
     await remove_banned_user(user_id)
-    await message.reply_sticker("CAACAgUAAxkBAAIjbGKPb3oOFXIT3KSxlIoefG7jTLOiAAJuBgAC_415VDBZlDYZrGxCJAQ")
+    await message.reply_sticker("CAACAgUAAxkBAAELKqVlojds6hKdx4JSESXI0B7OvfeTYAACcAYAAlDWyVSevja3qth4qjQE")
     await message.reply_text(
         _["gban_9"].format(mention, number_of_chats)
     )
